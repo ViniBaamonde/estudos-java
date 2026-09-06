2489acf5d83072da1eafa1a8db2be8ef94de5c76
Vinicius Baamonde <vinibonfim9312@gmail.com>
2026-09-06 01:47:39 -0300

    Adiciona exercício de pensionato usando vetores e índice posicional

---
+++ 03-pensionato-vetores/Aluguel.java
@@ -0,0 +1,26 @@
+package entities;
+
+public class Aluguel {
+
+	private String nome;
+	private String email;
+	
+	
+	
+	public Aluguel() {
+	}
+
+
+
+	public Aluguel(String nome, String email) {
+		this.nome = nome;
+		this.email = email;
+	}
+	
+	
+	public String toString() {
+		return nome + ", " + email;
+	}
+	
+	
+}
---
+++ 03-pensionato-vetores/Program.java
@@ -0,0 +1,48 @@
+package application;
+
+import java.util.Scanner;
+
+import entities.Aluguel;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		
+		System.out.print("Quantos quartos você deseja alugar: ");
+		int quantidade = sc.nextInt();
+		sc.nextLine();
+		
+		Aluguel [] reserva = new Aluguel[10];
+		
+		
+		for(int i = 0; i< quantidade; i++) {
+			
+			System.out.println("Reserva #" + (i+1) + ":");
+			System.out.print("Nome: ");
+			String nome = sc.nextLine();
+			System.out.print("Email: ");
+			String email = sc.nextLine();
+			System.out.print("Quarto: ");
+			int numQuarto = sc.nextInt();
+			
+			reserva [numQuarto] = new Aluguel(nome,email);
+			System.out.println();
+			sc.nextLine();
+		}
+		System.out.println();
+		
+		for(int i = 0; i < 10; i++) {
+			if(reserva[i] != null) {
+				System.out.println(i + " :" + reserva[i] );
+			}
+		}
+		
+		
+		
+		sc.close();
+	}
+
+}
